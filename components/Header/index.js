8fb269c97f57a639a6e00988ee4acdb34bad71d6
admin <admin@admins-MacBook-Pro.local>
2019-09-06 08:13:50 -0700

    header completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,29 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+function Header() {
+     const header = document.createElement('header');
+     const date = document.createElement('span');
+     const title = document.createElement('h1');
+     const temp = document.createElement('span')
+
+     headerContainer.appendChild(header);
+     header.appendChild(date);
+     header.appendChild(title);
+     header.appendChild(temp);
+
+
+     header.classList.add('header');
+     date.classList.add('date');
+     temp.classList.add('temp');
+
+     date.textContent = 'MARCH 28, 2019';
+     title.textContent = 'Lambda Times';
+     temp.textContent = '98';
+
+     return header
+}
+
+Header();
