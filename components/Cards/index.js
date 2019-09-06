8387685daadf93cd543f6bb3937fc557c2e1da32
admin <admin@admins-MacBook-Pro.local>
2019-09-06 09:18:16 -0700

    mvp complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,52 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+// we get { key: array / key: arrays / key: arrays }
+// access each KEY and loop through each ARRAY 
+
+const cardsContainer = document.querySelector('.cards-container');
+
+function Article(obj){
+     const card = document.createElement('div')
+     const headline = document.createElement('div')
+     const author = document.createElement('div')
+     const imageContainer = document.createElement('div')
+     const image = document.createElement('img')
+     const name = document.createElement('span')
+
+     card.appendChild(headline);
+     card.appendChild(author);
+     author.appendChild(imageContainer);
+     imageContainer.appendChild(image);
+     author.appendChild(name);
+
+     card.classList.add('card');
+     headline.classList.add('headline');
+     author.classList.add('author');
+     imageContainer.classList.add('img-container');
+
+     headline.textContent = obj.headline;
+     image.src = obj.authorPhoto;
+     name.textContent = obj.authorName;
+
+     return card
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+     .then(response => {
+          let artObjValues = Object.values(response.data.articles)    // returns an array of all the values 
+          artObjValues.forEach(index => {                             // loop through array of values to separate into individual arrays
+               index.forEach(value => {                               // each array contains objects - loop through that array
+                    // console.log(value);
+                    let card = Article(value);
+                    cardsContainer.appendChild(card);
+               })
+          })
+     })
+     .catch(error => {
+          console.log("Your data was not returned", error);
+     })
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -16,12 +16,15 @@ const tabContainer = document.querySelector('.topics');
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
 .then(response => {
      response.data.topics.forEach(item => {
-          console.log(item);
+          // console.log(item);
 
           let topics = document.createElement('div');
           topics.classList.add('tab');
           topics.textContent = item;
           tabContainer.appendChild(topics)
      })
+})
+.catch(error => {
+     console.log("Your data was not returned", error);
 })
 
